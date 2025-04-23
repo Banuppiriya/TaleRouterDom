9ec62ba98e4f3bf309f69b8575a92355c51abecc
Your Name <theepika0028@gmail.com>
2025-04-24 01:15:05 +0530

    next commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/TaleRouterDom",
+  base: '/TaleRouterDom/', // This must match your repo name on GitHub
   plugins: [react()],
 })
